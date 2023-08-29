a3932cc7516d3b33989228af19206970533d58f8
Ronaldo César <Ronaldo Csar@NOTEBOOK-I3>
2023-08-29 19:07:06 -0300

    Adicionar arquivos de projeto.

---
+++ TABUADA/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Tabuada
+{
+    public static List<int> CalcularTabuada(int numero)
+    {
+        List<int> tabuada = new List<int>();
+        for (int i = 1; i <= 10; i++)
+        {
+            tabuada.Add(numero * i);
+        }
+        return tabuada;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numeroDesejado = 18;
+        List<int> tabuadaDezoito = Tabuada.CalcularTabuada(numeroDesejado);
+
+        Console.WriteLine($"Tabuada do número {numeroDesejado}:");
+        foreach (int resultado in tabuadaDezoito)
+        {
+            Console.WriteLine(resultado);
+        }
+    }
+}
